cb8ae7dcf8442913f56777a8d27ce8a487e2caae
joelvegas20 <joel19vegas@gmail.com>
2025-01-22 13:07:05 -0500

    fix: add url search params to create collection

--- src/app/create-collection/page.tsx
+++ src/app/create-collection/page.tsx
@@ -1,21 +1,22 @@
 "use client"
+
 import React, { useRef, useState, useEffect } from 'react';
-import { useSearchParams } from 'next/navigation';
 import { PhotoIcon, XMarkIcon } from '@heroicons/react/24/solid';
 import { createCollection } from '@/lib/createCollection';
 import { ToastContainer, toast } from 'react-toastify';
 
 const CreateCollection = () => {
-  const [notificationHasBeenShown, setNotificationHasBeenShown] = useState<boolean>(false);
-  const searchParams = useSearchParams();
-  const notify = searchParams.get('notify');
+  const [notificationHasBeenShown, setNotificationHasBeenShown] = useState(false);
   const [collectionName, setCollectionName] = useState('');
   const [collectionQuantity, setCollectionQuantity] = useState('');
   const [collectionDescription, setCollectionDescription] = useState('');
   const [collectionImage, setCollectionImage] = useState<File | null>(null);
   const [previewURL, setPreviewURL] = useState<string | null>(null);
   const fileInputRef = useRef<HTMLInputElement>(null);
+
   useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const notify = params.get('notify');
     if (!notificationHasBeenShown && notify === 'true') {
       setNotificationHasBeenShown(true);
       toast.info('You need to create a collection first!', {
@@ -23,20 +24,21 @@ const CreateCollection = () => {
         autoClose: 3000,
       });
     }
-  }, [notify, notificationHasBeenShown]);
+  }, [notificationHasBeenShown]);
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    // verify if the collection quantity is a number
     if (isNaN(Number(collectionQuantity))) {
       alert('Collection quantity must be a number');
       return;
     }
     await createCollection({ collectionName, collectionDescription, collectionImage });
   };
+
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]) {
       const file = e.target.files[0];
-      setCollectionImage(e.target.files[0]);
+      setCollectionImage(file);
       const reader = new FileReader();
       reader.onload = () => {
         setPreviewURL(reader.result as string);
@@ -44,6 +46,7 @@ const CreateCollection = () => {
       reader.readAsDataURL(file);
     }
   };
+
   return (
     <div className="flex justify-center mt-4">
       <form
@@ -54,9 +57,7 @@ const CreateCollection = () => {
           <ToastContainer position="top-left" />
         </div>
         <h1 className="text-center text-3xl">Create a new collection</h1>
-        {/* container of two columns */}
         <div className="flex flex-row w-full gap-10">
-          {/* first column */}
           <div className="flex flex-col columna-1 w-1/2">
             <div className="flex flex-col">
               <label htmlFor="collection-name" className="mt-4">Collection name</label>
@@ -91,51 +92,43 @@ const CreateCollection = () => {
               />
             </div>
           </div>
-          {/* second column */}
           <div className="flex flex-col w-1/2">
             <label htmlFor="collection-image" className="mt-4 text-center">Collection image</label>
             <div className="flex flex-col items-center justify-center bg-gray-200 p-2 text-black rounded h-full">
               <label htmlFor="collection-image" className="cursor-pointer flex flex-col items-center">
-                {
-                  !previewURL && (
-                    <>
-                      <PhotoIcon width={80} />
-                      <span className="mt-2 text-gray-500">Click to upload an image</span>
-                    </>
-                  )
-                }
+                {!previewURL && (
+                  <>
+                    <PhotoIcon width={80} />
+                    <span className="mt-2 text-gray-500">Click to upload an image</span>
+                  </>
+                )}
               </label>
               <input
                 type="file"
                 id="collection-image"
                 className="hidden"
-                accept='image/*'
+                accept="image/*"
                 onChange={handleImageChange}
                 ref={fileInputRef}
                 required
               />
-              {
-                previewURL && (
-                  <figure className='relative'>
-                    <img
-                      src={previewURL}
-                      alt="Image Preview"
-                    />
-                    <button
-                      className="absolute bg-gray-300 hover:bg-gray-400 hover:text-black transition-all ease-in-out top-0 right-0 rounded-full text-black"
-                      onClick={() => {
-                        setPreviewURL(null);
-                        setCollectionImage(null);
-                        if (fileInputRef.current) {
-                          fileInputRef.current.value = '';
-                        }
-                      }}
-                    >
-                      <XMarkIcon width={24} />
-                    </button>
-                  </figure>
-                )
-              }
+              {previewURL && (
+                <figure className="relative">
+                  <img src={previewURL} alt="Image Preview" />
+                  <button
+                    className="absolute bg-gray-300 hover:bg-gray-400 hover:text-black transition-all ease-in-out top-0 right-0 rounded-full text-black"
+                    onClick={() => {
+                      setPreviewURL(null);
+                      setCollectionImage(null);
+                      if (fileInputRef.current) {
+                        fileInputRef.current.value = '';
+                      }
+                    }}
+                  >
+                    <XMarkIcon width={24} />
+                  </button>
+                </figure>
+              )}
             </div>
           </div>
         </div>
@@ -148,6 +141,6 @@ const CreateCollection = () => {
       </form>
     </div>
   );
-}
+};
 
 export default CreateCollection;
